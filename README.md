94a9d0e039f1a47d3df0ae0d10440f4568c106b4
kenpeter <figo2476@hotmail.com>
2016-12-15 21:59:54 +1100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+## Intro
+
+Tutorial: https://www.youtube.com/watch?v=JPT3bFIwJYA&index=1&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS
+
+## Install
+
+
+## Run
+
+
+
+## App
+
+
+## What I learned
+
+ 
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require("webpack");
+var path = require("path");
+
+// create auto dist
+// create dist path
+var DIST_DIR = path.resolve(__dirname, "dist");
+
+// create src path
+var SRC_DIR = path.resolve(__dirname, "src");
+
+
+var config = {
+	// ./src/app/index.js
+	entry: SRC_DIR + "/app/index.js", 
+
+	// ./dist/app/bundle.js
+	output: {
+		path: DIST_DIR + "/app",
+		filename: "bundle.js",
+		publicPath: "/app/"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?/,
+				include: SRC_DIR,
+				loader: "babel-loader",
+				query: {
+					presets: ["react", "es2015", "stage-2"]
+				} 
+			}	
+		]
+
+	}
+
+};
+
+
+module.exports = config;
